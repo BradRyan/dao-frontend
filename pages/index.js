164cade80e14ace28ef1bdc5efe629f6aa4fd952
Bradley Ryan <bradryan7@gmail.com>
2022-02-28 21:35:50 -0800

    basic contract integration

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,63 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { useEffect, useState } from "react";
+import Head from "next/head";
+import Image from "next/image";
+import Web3Modal from "web3modal";
+import { ethers } from "ethers";
+import styled from "styled-components";
+import axios from "axios";
+
+import styles from "../styles/Home.module.css";
+
+import KiftVans from "../config/KiftVans.json";
+
+const KIFT_VANS_CONTRACT_ADDRESS = "0xbaa1763C83063fe8571Ff8A8D8AD903CF1DA94d9";
+
+const rpcEndpoint = "https://rpc-mumbai.matic.today";
+
+const getImageData = (tokenId) => {
+  const CONTENT_ID = "QmNbpCqZhXVLhddRSapVtST5jkvxo2VAJ4WtT5U7PyR8pt";
+
+  const metadataURI = `https://gateway.pinata.cloud/ipfs/${CONTENT_ID}/${tokenId}.json`;
+  const imageURI = `https://gateway.pinata.cloud/ipfs/${CONTENT_ID}/${tokenId}.png`;
+  return { metadataURI, imageURI };
+};
 
 export default function Home() {
+  const [balance, setBalance] = useState();
+  const [myNfts, setMyNfts] = useState([]);
+
+  async function getBalance() {
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+
+    const [account] = await connection.request({
+      method: "eth_requestAccounts",
+    });
+
+    const balance = await provider.getBalance(account);
+    setBalance(ethers.utils.formatEther(balance));
+  }
+
+  async function getNfts() {
+    // provider is the blockchain
+    const provider = new ethers.providers.JsonRpcProvider(rpcEndpoint);
+
+    // get the smart contract
+    const contract = new ethers.Contract(
+      KIFT_VANS_CONTRACT_ADDRESS,
+      KiftVans.abi,
+      provider
+    );
+    const count = await contract.count();
+
+    console.warn({ contract, count: parseInt(count) });
+  }
+
+  useEffect(() => {
+    getNfts();
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
@@ -11,44 +66,27 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <div>
+        <h5>Your Balance: {balance}</h5>
+        <button onClick={() => getBalance()}>Show My Balance</button>
+        {/* <button onClick={() => mintNft()}>Mint a van!</button> */}
+      </div>
+
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to the <a href="https://nextjs.org">Kift DAO</a>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <p className={styles.description}>Get started by minting a van!</p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          {Array(10)
+            .fill(0)
+            .map((_, i) => (
+              <div key={i} className={styles.card}>
+                <NFTImage tokenId={i + 1} />
+              </div>
+            ))}
         </div>
       </main>
 
@@ -58,12 +96,96 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
+}
+
+const getMintedStatus = async (metadataURI) => {
+  const web3Modal = new Web3Modal();
+  const connection = await web3Modal.connect();
+  const provider = new ethers.providers.Web3Provider(connection);
+  // const provider = new ethers.providers.JsonRpcProvider(rpcEndpoint);
+  const contract = new ethers.Contract(
+    KIFT_VANS_CONTRACT_ADDRESS,
+    KiftVans.abi,
+    provider
+  );
+
+  const isMinted = await contract.isContentOwned(metadataURI);
+  if (isMinted) console.warn({ isMinted, metadataURI });
+  return isMinted;
+};
+
+async function mintNFT(metadataURI) {
+  const web3Modal = new Web3Modal();
+  const connection = await web3Modal.connect({
+    // cacheProvider: false,
+  });
+  const provider = new ethers.providers.Web3Provider(connection);
+  const signer = provider.getSigner();
+
+  const contract = new ethers.Contract(
+    KIFT_VANS_CONTRACT_ADDRESS,
+    KiftVans.abi,
+    signer
+  );
+
+  // TODO: should payToMint should use msg.sender vs take the address as an argument
+  const signerAddress = await signer.getAddress();
+  const result = await contract.payToMint(signerAddress, metadataURI, {
+    value: ethers.utils.parseEther("0.05"),
+  });
+
+  console.warn({ result });
+  return result;
+}
+
+function NFTImage({ tokenId }) {
+  const [isMinted, setIsMinted] = useState(false);
+  const [meta, setMeta] = useState({});
+  const { metadataURI, imageURI } = getImageData(tokenId);
+
+  useEffect(() => {
+    if (!metadataURI) return;
+
+    getMintedStatus(metadataURI).then((data) => {
+      // debugger;
+      setIsMinted(data);
+    });
+  }, [metadataURI]);
+
+  useEffect(() => {
+    if (!metadataURI) return;
+
+    axios.get(metadataURI).then(({ data }) => {
+      setMeta(data);
+    });
+  }, [metadataURI]);
+
+  return (
+    <div className="card">
+      <img src={imageURI} style={{ width: "100%" }} />
+
+      <div className="card-body">
+        <h5 className="card-title">ID #{tokenId}</h5>
+        <h5>{meta.name}</h5>
+        <div>{meta.description}</div>
+        {isMinted ? (
+          <div>Minted!</div>
+        ) : (
+          <button
+            onClick={() => mintNFT(metadataURI).then(() => setIsMinted(true))}
+          >
+            Mint
+          </button>
+        )}
+      </div>
+    </div>
+  );
 }
