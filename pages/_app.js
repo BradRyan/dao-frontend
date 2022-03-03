5a8a30af56a2f6d067e2321e1877fad7249967e4
Bradley Ryan <bradryan7@gmail.com>
2022-03-03 15:18:49 -0800

    use 3rdweb hook for login for ease of use

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,32 @@
-import '../styles/globals.css'
+import { ThirdwebProvider } from "@3rdweb/react";
+import "../styles/globals.css";
+
+// Put the ethereum chain ids of the chains you want to support
+const mumbaiChainId = 80001;
+const ethMainNetChainId = 1;
+const ethRinkebyChainId = 4;
+const polygonMainNetChainId = 137;
+const supportedChainIds = [
+  ethMainNetChainId,
+  ethRinkebyChainId,
+  polygonMainNetChainId,
+  mumbaiChainId,
+];
+
+const connectors = {
+  // metamask
+  injected: {},
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThirdwebProvider
+      connectors={connectors}
+      supportedChainIds={supportedChainIds}
+    >
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
